81202c75654d5da1b628365966fe67700749f328
Dmitry Boldyrev <ribkatt@gmail.com>
2016-08-12 00:57:08 +0300

    Remove unused classes

--- build/clean/configcheck.js
+++ build/clean/configcheck.js
@@ -1,8 +1,6 @@
 "use strict";
-const R = require('ramda');
 const inspector = require('schema-inspector');
 const config_example_1 = require('./config-example');
-const task_1 = require('./task/task');
 const project_1 = require('./task/project');
 const Prop = require('./package/props-type');
 const validmodel_1 = require('./task/validmodel');
@@ -21,38 +19,11 @@ ResultConfigCheck.ResultCheck = {
     name: ResultConfigCheck.stringCheck([''])
 };
 ResultConfigCheck.validate = (obj) => inspector.validate(validmodel_1.ValidatorModel.ResultConfig, obj).valid;
-class SourceConfig {
-    static splitProjects(obj) {
-        let projectMap = new Map();
-        R.forEach(e => projectMap.set(e[0], SourceConfig.childMap(e[1])), R.toPairs(obj));
-        return projectMap;
-    }
-    static childMap(obj) {
-        let childMap = new Map();
-        R.forEach(e => childMap.set(e[0], e[1]), R.toPairs(obj));
-        return childMap;
-    }
-    static onEveryTask(project, func) {
-        let result = new Map();
-        const createSubproject = (subname, taskname, task) => R.when(() => R.not(result.has(subname)), result.set(subname, new Map()));
-        const createTask = (subname, taskname, task) => {
-            createSubproject(subname, taskname, task);
-            console.assert(result.has(subname), `${subname} subtask not created! Task: ${taskname}`);
-            return (_func) => result.get(subname).set(taskname, _func(subname, taskname, task));
-        };
-        project.forEach((sub, subname) => sub.forEach((task, taskname) => createTask(subname, taskname, task)(func)));
-        return result;
-    }
-}
-SourceConfig.ConstructTask = (subname, taskname, obj) => R.constructN(3, task_1.FullTask)(subname, taskname, obj);
-let projectSplit = SourceConfig.splitProjects(source);
-let withNames = SourceConfig.onEveryTask(projectSplit, SourceConfig.ConstructTask);
-let styl = withNames.get('client').get('stylus');
-log.tags(['styl object', 'pipes']).debug(styl.pipes);
 let conf = project_1.Projectlist.configSplitter(source);
-console.info(conf);
 conf.render();
+conf.get('client').run();
 log.log(gulp.hasTask('client:stylus'));
 log.log(gulp.hasTask('client'));
+console.info(conf);
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {};
--- source/configcheck.ts
+++ source/configcheck.ts
@@ -3,7 +3,7 @@ import * as R from 'ramda'
 const inspector = require('schema-inspector')
 // import { Map } from 'typescript'
 import { sample as config } from './config-example'
-import { FullTask } from './task/task'
+// import { FullTask } from './task/task'
 import { Projectlist } from './task/project'
 
 import * as Prop from './package/props-type'
@@ -29,54 +29,15 @@ class ResultConfigCheck {
     }
     public static validate = (obj:Object):boolean=>inspector.validate(ValidatorModel.ResultConfig,obj).valid
 }
-type Task = R.Dictionary<Object|String|Array<any>|Number>
-type TSubproject = Map<Task>
-type TProject = Map<TSubproject>
-class SourceConfig {
-    public static splitProjects(obj:Object) {
-        let projectMap:Map<any> = new Map<any>()
-        R.forEach(e=>projectMap.set(e[0],SourceConfig.childMap(e[1])),R.toPairs(obj))
-        return projectMap
-    }
-    private static childMap(obj:Object) {
-        let childMap = new Map<Object>()
-        R.forEach(e=>childMap.set(e[0],e[1]),R.toPairs(obj))
-        return childMap
-    }
-    public static onEveryTask<T>(project:TProject,func:(subname:string,taskname:string,obj:Object)=>T):TProject {
-        let result = new Map<Object>()
-        const createSubproject = (subname:string,taskname:string,task:Object)=>
-            R.when(
-                ()=>R.not(result.has(subname)),
-                    result.set(subname,new Map<Object>()))
-        const createTask = (subname:string,taskname:string,task:Object)=>{
-            createSubproject(subname,taskname,task)
-            console.assert(result.has(subname),`${subname} subtask not created! Task: ${taskname}`)
-            return (_func)=>result.get(subname).set(taskname,_func(subname,taskname,task))
-        }
-        project.forEach(
-            (sub:Object,subname:string)=>sub.forEach(
-                (task:Object,taskname:string)=>
-                    createTask(subname,taskname,task)(func)))
-        return result
-    }
-    public static ConstructTask = (subname:string,taskname:string,obj:Object)=>
-        R.constructN(3,FullTask)(subname,taskname,obj)
-}
 
-let projectSplit = SourceConfig.splitProjects(source)
-let withNames = SourceConfig.onEveryTask(projectSplit,SourceConfig.ConstructTask)
-let styl:FullTask = withNames.get('client').get('stylus')
-// let client:Project = new Project(withNames.get('client').values(),'client')
-log.tags(['styl object','pipes']).debug(styl.pipes)
 // client.run()
 // styl.run()
 let conf = Projectlist.configSplitter(source)
-console.info(conf)
 conf.render()
-// conf.run()
+conf.get('client').run()
 log.log(gulp.hasTask('client:stylus'))
 log.log(gulp.hasTask('client'))
+console.info(conf)
 // console.log(inspector.validate(ResultConfigCheck.sanit,source))
 
 export default {}
